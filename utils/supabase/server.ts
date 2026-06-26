8054e5869e9be850567a3cb103f8699692fb097a
sumbatimur <gresanthobire89@gmail.com>
2026-06-27 01:38:15 +0800

    Fix : Server.ts

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,8 +1,9 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
-  const cookieStore = cookies()
+export async function createClient() {
+  const cookieStore = await cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -17,7 +18,7 @@ export function createClient() {
               cookieStore.set(name, value, options)
             )
           } catch {
-            // Server Component, ignore
+            // The cookieStore can only be mutated in a Server Action or Route Handler
           }
         },
       },
